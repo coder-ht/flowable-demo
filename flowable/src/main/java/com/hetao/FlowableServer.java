50b6f1e1f397c39c8e6516819f3fdb562c1fb495
hetao <2990096445@qq.com>
2025-09-26 15:41:23 +0800

    测试-1

--- flowable/src/main/java/com/hetao/FlowableServer.java
+++ flowable/src/main/java/com/hetao/FlowableServer.java
@@ -1,4 +1,4 @@
-package personal.hetao;
+package com.hetao;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
@@ -6,6 +6,6 @@ import org.springframework.boot.autoconfigure.SpringBootApplication;
 @SpringBootApplication
 public class FlowableServer {
     public static void main(String[] args) {
-        SpringApplication.run(FlowableServer.class);
+        SpringApplication.run(FlowableServer.class, args);
     }
 }
--- flowable/src/test/java/FlowableTest.java
+++ flowable/src/test/java/FlowableTest.java
@@ -1,10 +1,60 @@
+import org.flowable.engine.RepositoryService;
+import org.flowable.engine.RuntimeService;
+import org.flowable.engine.TaskService;
+import org.flowable.engine.repository.Deployment;
+import org.flowable.engine.repository.ProcessDefinition;
+import org.flowable.engine.runtime.ProcessInstance;
+import org.flowable.task.api.Task;
 import org.junit.jupiter.api.Test;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.context.SpringBootTest;
+import com.hetao.FlowableServer;
 
-@SpringBootTest
+import java.util.List;
+
+@SpringBootTest(classes = FlowableServer.class)
 public class FlowableTest {
+    /**
+     * 部署流程相关
+     */
+    @Autowired
+    private RepositoryService repositoryService;
+    /**
+     * 创建实例相关
+     */
+    @Autowired
+    private RuntimeService runtimeService;
+    /**
+     * 任务相关
+     */
+    @Autowired
+    private TaskService taskService;
     @Test
     public void test() {
-        System.out.println("hello");
+        //启动流程
+        Deployment deployment = repositoryService.createDeployment()
+                .addClasspathResource("first.bpmn20.xml")
+                .name("第一个部署流程")
+                .deploy();
+
+        repositoryService.createProcessDefinitionQuery().deploymentId(deployment.getId()).singleResult();
+        System.out.println("流程id:" + deployment.getId());
+        ProcessDefinition processDefinition = repositoryService.createProcessDefinitionQuery().deploymentId(deployment.getId())
+                .singleResult();
+        System.out.println("流程定义id："+processDefinition.getId());
+        //创建实例
+        ProcessInstance processInstance = runtimeService.startProcessInstanceById(processDefinition.getId());
+        System.out.println("流程实例id:" + processInstance.getId());
+        //查询任务
+        List<Task> list = taskService.createTaskQuery().processInstanceId(processInstance.getId()).list();
+        //完成任务
+        list.forEach(task -> {
+            System.out.println("任务id:" + task.getId());
+            System.out.println("任务名称:" + task.getName());
+            System.out.println("任务创建时间:" + task.getCreateTime());
+            System.out.println("任务办理人:" + task.getAssignee());
+            taskService.complete(task.getId());
+            System.out.println("完成任务:" + task.getName());
+        });
     }
 }
